80dd954cee3c5acbf66bd9245775e8931fae92da
Tomasz Chełmecki <tomaszchelmecki123@gmail.com>
2023-09-01 12:01:43 +0200

    Creating a frame of a single product in the file main-content.js

---
+++ js/main-content.js
@@ -0,0 +1,90 @@
+{/* <div class="main-content">
+                <div class="element1">
+                    <div class="title-element1">
+                        <span>Pralka SANSUNG WW80TA02341FAE/EA Ecobubble</span> <span>SANSUNG</span> 
+                        <div class="stars-element1">
+                            <ion-icon name="star"></ion-icon>
+                            <ion-icon name="star"></ion-icon>
+                            <ion-icon name="star"></ion-icon>
+                            <ion-icon name="star"></ion-icon>
+                            <ion-icon name="star"></ion-icon>
+                            <span>(3 opinie)</span> <span>nr kat. 1424448</span>
+                        </div>
+                        <div class="big-info">
+                            <div class="big-info-title">Wielka wyprzedaż. Tysiące produktów TANIEJ</div>
+                        </div>
+                        <div class="device">
+                            <div class="device-img">
+                                <img src="img/sansung1.webp" alt="">
+                            </div>
+                            <div class="device-info">
+                                Wymiary: <b>(szer. x wys. x gł.): 60 x 85 x 55 cm</b><br>
+                                Wsad [kg]: <b>8</b> <br>
+                                Kolor: <b>Biały, Czarne drzwi</b> <br>
+                                Maksymalna prędkość wirowania [obr/min]: <b>1200</b> <br>
+                                Wyświetlacz elektroniczny: <b>TAK</b> <br>
+                                Zabezpieczenia: <b>Blokada rodzicielska</b> <br>
+                                Koszt zużycia prądu na 100 cykli: <b>55 kWh = 42.35 PLN</b> <br>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                   
+                </div>
+                <div class="element2">
+                    <p>1979,-</p>
+                </div>
+</div> */}
+
+const currentProducts = products;
+
+const productsSection = document.querySelector(".right-content");
+
+const renderProducts = (items) => {
+    for(let i = 0; i < items.length; i++){
+        const newProduct = document.createElement('div');
+        newProduct.className = 'main-content';
+        newProduct.innerHTML = `
+        <div class="element1">
+                    <div class="title-element1">
+                        <span>${items[i].name}</span>
+                        <div class="stars-element1">
+                            <ion-icon name="star"></ion-icon>
+                            <ion-icon name="star"></ion-icon>
+                            <ion-icon name="star"></ion-icon>
+                            <ion-icon name="star"></ion-icon>
+                            <ion-icon name="star"></ion-icon>
+                            <span>(3 opinie)</span> <span>nr kat. 1424448</span>
+                        </div>
+                        <div class="big-info">
+                            <div class="big-info-title">Wielka wyprzedaż. Tysiące produktów TANIEJ</div>
+                        </div>
+                        <div class="device">
+                            <div class="device-img">
+                                <img src="${items[i].image}" alt="product-image">
+                            </div>
+                            <div class="device-info">
+                               ${items[i].description}
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                   
+                </div>
+                <div class="element2 on-sale" >
+                    <p class="price-sale">${items[i].price - items[i].saleAmount},-</p>
+                    <p class="price-basic">${items[i].price}zł</p>
+                    <p class="days30">Najniższa cena z 30 dni przed obniżką: ${items[i].price},-</p>
+                    <div class="element2-btn">
+                    <button>DO KOSZYKA</button>
+                    </div>
+        </div>
+        `
+
+        productsSection.appendChild(newProduct);
+    }
+};
+
+document.onload = renderProducts(currentProducts);
---
+++ js/products.js
@@ -0,0 +1,54 @@
+const products = [
+    {
+      id: 0,
+      name: "Pralka SANSUNG WW80TA02341FAE/EA Ecobubble",
+      description: ` Wymiary: <b>(szer. x wys. x gł.): 60 x 85 x 55 cm</b><br>
+      Wsad [kg]: <b>8</b> <br>
+      Kolor: <b>Biały, Czarne drzwi</b> <br>
+      Maksymalna prędkość wirowania [obr/min]: <b>1200</b> <br>
+      Wyświetlacz elektroniczny: <b>TAK</b> <br>
+      Zabezpieczenia: <b>Blokada rodzicielska</b> <br>
+      Koszt zużycia prądu na 100 cykli: <b>55 kWh = 42.35 PLN</b> <br>`,
+      category: "pralka",
+      price: 1979,
+      image: "/img/sansung1.webp",
+      sale: true,
+      saleAmount: 200,
+    },
+    {
+      id: 1,
+      name: "Lodówka WHIRLOOp ZKO14O",
+      description: `Wymiary: (szer. x wys. x gł.): 91.2 x 178 x 71.6 cm
+      Pojemność netto chłodziarki/zamrażarki: 389l / 225l
+      Kolor: Grafitowa stal
+      Poziom hałasu [dB]: 36
+      Kostkarka: Tak
+      Dystrybutor wody: Tak
+      Roczny koszt zużycia prądu: 346 kWh = 266.42 PLN
+      Zastosowane technologie: All Around Cooling, Bixby, Bluetooth Call, <br>
+      Family Hub™, Funkcja View Inside, Łączność Wi-Fi, No-Frost,<br> SmartThings, SmartThings (kamery), SmartView telefonu / telewizora,<br> 
+      Tryb wakacyjny, Twin Cooling Plus`,
+      category: "iphone",
+      price: 6319,
+      image: "/img/fridge.webp",
+      sale: true,
+      saleAmount: 200,
+    },
+    {
+      id: 2,
+      name: "Robot BOCHS OP234WQ",
+      description: `Moc [W]: 1000
+      Pojemność misy roboczej [l]: 3.9
+      Mikser kielichowy: TAK
+      Załączone wyposażenie: Blender kielichowy, Do pobrania książka z <br> 
+      przepisami kulinarnymi, Dwustronna tarcza cienkie/grube plasterki, <br> 
+      Dwustronna tarcza do wiórek cienkich/grubych, Hak do wyrabiania <br> 
+      ciasta, Końcówka do mieszania, Końcówka do ubijania, Maszynka do <br>
+      mielenia mięsa, Osłona przeciwko chlapaniu, Tarcza do tarcia na grubo, Tarcza do tarcia – średnio, Wyciskarka do owoców cytrusowych`,
+      category: "robot",
+      price: 1169,
+      image: "/img/robot.webp",
+      sale: true,
+      saleAmount: 451,
+    },
+  ];
